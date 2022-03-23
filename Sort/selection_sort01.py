6d8c3ed3daffca885957242272064b755bf31c09
hwanyo14 <hwanyo14@gmail.com>
2022-03-23 18:00:25 +0900

    22/03/23

---
+++ Sort/selection_sort01.py
@@ -0,0 +1,16 @@
+'''
+  <선택 정렬>
+  
+    가장 작은 데이터를 선택하여 맨 앞의 데이터와 스왑(swap)하고,
+    그 다음으로 작은 수를 선택해 앞에서 두 번째로 작은 데이터와 스왑한다.
+'''
+array = [7,5,9,0,3,1,6,2,4,8]
+
+for i in range(len(array)):
+  min_idx = i
+  for j in range(i + 1, len(array)):
+    if array[min_idx] > array[j]:
+      min_idx = j
+  array[i], array[min_idx] = array[min_idx], array[i]
+  
+print(array)      
---
+++ Sort/sort02.py
@@ -0,0 +1,28 @@
+'''
+  <성적이 낮은 순서로 학생 출력하기>
+  
+    N명의 학생 정보가 있다.
+    학생 정보는 이름과 성적으로 구분된다.
+    이때 성적이 낮은 순서대로 학생의 이름을 출력하라.
+    
+    [input]
+      1. 첫 번째 줄에 학생수 N
+      2. 학생의 이름 A와 성적 B를 공백으로 구분하여 입력
+      
+    [output]
+      1. 성적 순으로 이름 출력
+      
+    ex> 2
+        홍길동 95
+        이순신 77
+'''
+n = int(input())
+array = []
+
+for i in range(n):
+  data = input().split()
+  array.append(data)
+  
+array.sort(key=lambda x: x[1])
+
+print(array)  
